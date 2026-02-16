4fa177d6904a25904ada85dba29249ec7834fa86
Rushi Gokani <rushigokani124@gmail.com>
2026-02-16 20:09:47 +0530

    feat: Add new image slider section, header logo block, and wave transition section.

--- assets/image-slider.js
+++ assets/image-slider.js
@@ -40,24 +40,6 @@ if(!customElements.get('image-slider')) {
           nextEl: this.querySelector('.swiper-button--next'),
           prevEl: this.querySelector('.swiper-button--prev')
         },
-        pagination: {
-          el: this.querySelector('.swiper-pagination'),
-          clickable: true,
-          renderBullet: (i, className) => {
-            const totalSlides = this.querySelectorAll('.swiper-slide').length;
-            return `
-            <button class="${className}">
-              <span>0${i + 1}</span>
-              <svg class="square-progress" width="26" height="26">
-                <rect class="square-origin" width="26" height="26" rx="5" ry="5" />
-              </svg>
-              <svg class="progress" width="18" height="18" style="inset-inline-start: ${0 - ((i * 2.4) + 3.4)}rem">
-                <circle class="circle-origin" r="8" cx="9.5" cy="9.5"></circle>
-              </svg>
-            </button>
-            `;
-          }
-        },
         autoplay: this.dataset.autoplay === 'true' ? autoplayOptions : false
       });
     }
